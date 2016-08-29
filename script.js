fb1046748dc72ac446c03832362de45716206b2f
jemilox <jemilox@gmail.com>
2016-08-28 21:38:33 -0500

    final product!

--- script.js
+++ script.js
@@ -1,62 +1,83 @@
 console.log("js sourced");
 
 var employees = [];
+var totalSalary = 0;
 
 //get employee information from Dom
 var getEmployeeInfo = function() {
 
-var totalSalary = 0;
-//create newEmployee variable from Dom input
-var newEmployee = {
 
-  fName : document.getElementById("fNameIn").value,
-  lName : document.getElementById("lNameIn").value,
-  id : document.getElementById("idIn").value,
-  jobTitle : document.getElementById("jobTitleIn").value,
-  annualSalary : Number(document.getElementById("annualSalaryIn").value)
+  //create newEmployee variable from Dom input
+  var newEmployee = {
+
+    fName : document.getElementById("fNameIn").value,
+    lName : document.getElementById("lNameIn").value,
+    id : document.getElementById("idIn").value,
+    jobTitle : document.getElementById("jobTitleIn").value,
+    annualSalary : Number(document.getElementById("annualSalaryIn").value)
+  };
+
+  //clear all text input
+  document.getElementById("fNameIn").value = "";
+  document.getElementById("lNameIn").value = "";
+  document.getElementById("idIn").value = "";
+  document.getElementById("jobTitleIn").value = "";
+  document.getElementById("annualSalaryIn").value = "";
+
+  console.log(newEmployee);
+
+  //create new employees array and push each employee
+  employees.push(newEmployee);
+
+  console.log(employees);
+
+  displayEmployees();
+}; //end getEmployeeInfo
+
+var displayEmployees = function(){
+totalSalary = 0;
+  //post employees to Dom
+  //clear employees
+  document.getElementById('showInfo').innerHTML = "";
+    //add employees
+    for (var i = 0; i < employees.length; i++) {
+      //add employee to Dom
+      var allEmployees = "<p>" + "Employee Name: " + employees[i].fName + " " + employees[i].lName + "<br>" +
+                    "ID: " + employees[i].id + "<br>" +
+                    "Job Title: " + employees[i].jobTitle + "<br>" +
+                    "Annual Salary: " + employees[i].annualSalary + "<br><br>" +
+                    "<button onClick='removeEmployee(" + i + ")'>Remove Employee</button" + "</p>";
+
+      document.getElementById('showInfo').innerHTML += allEmployees;
+
+
+
+      document.getElementById('totalSalary').innerHTML = "";
+      //compute total Salary
+      totalSalary += employees[i].annualSalary;
+      //convert to currency
+
+
+      console.log(totalSalary);
+
+      //add total Salary on Dom
+
+
+    }
+  totalSalary = totalSalary.toLocaleString('USD', {style: 'currency', currency: 'USD'});
+
+  document.getElementById('totalSalary').innerHTML = totalSalary;
 };
 
-//clear all text input
-document.getElementById("fNameIn").value = "";
-document.getElementById("lNameIn").value = "";
-document.getElementById("idIn").value = "";
-document.getElementById("jobTitleIn").value = "";
-document.getElementById("annualSalaryIn").value = "";
 
-console.log(newEmployee);
+//remove single employee from the Dom with button
+var removeEmployee = function(index){
+  console.log('in removeEmployee');
 
-//create new employees array and push each employee
-employees.push(newEmployee);
+  var removedEmployee = employees[index];
 
-console.log(employees);
+  employees.splice(index, 1);
 
-//post employees to Dom
-//clear employees
-document.getElementById('showInfo').innerHTML = "";
-//add employees
-for (var i = 0; i < employees.length; i++) {
-//add employee to Dom
-var allEmployees = "<p>" + "Employee Name: " + employees[i].fName + " " + employees[i].lName + "<br>"
-                  + "ID: " + employees[i].id + "<br>" +
-                  "Job Title: " + employees[i].jobTitle + "<br>" +
-                  "Annual Salary: " + employees[i].annualSalary + "<br>" + "</p>";
-                  "<button onClick="
+  displayEmployees();
 
-document.getElementById('showInfo').innerHTML += allEmployees;
-
-//compute total Salary
-totalSalary += employees[i].annualSalary;
-//convert to currency
-totalSalary = totalSalary.toLocaleString('USD', {style: 'currency', currency: 'USD'});
-
-console.log(totalSalary);
-
-//clear totalSalary on Dom
-document.getElementById('totalSalary').innerHTML = '';
-
-//add total Salary on Dom
-document.getElementById('totalSalary').innerHTML = totalSalary
-
-}
-
-}; //end newEmployee
+};//end removeEmployee
